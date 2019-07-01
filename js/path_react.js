2aa7696212d4e807d92054560a1cf66b42e150f5
chanel-bofa <52410735+chanel-bofa@users.noreply.github.com>
2019-07-01 10:27:52 -0400

    Update path_react.js

--- js/path_react.js
+++ js/path_react.js
@@ -1,13 +1,3 @@
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
-
 class Component extends React.Component{
 	render(){
 		return (
@@ -21,6 +11,22 @@ class Component extends React.Component{
 }
 
 class Path extends React.Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	    	newRole: this.getParameterByName('newRole') ,
+		oldRole: this.getParameterByName('oldRole'),
+	    };
+	}
+	getParameterByName(name, url) {
+	    if (!url) url = window.location.href;
+	    name = name.replace(/[\[\]]/g, '\\$&');
+	    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+		results = regex.exec(url);
+	    if (!results) return null;
+	    if (!results[2]) return '';
+	    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+	}
 	renderComponent(arg, isArrow){
 		return <Component isArrow={isArrow} arg={arg}>;
 	}
