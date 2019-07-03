4f31a0508d46075f170b7a963bc94b681749c98f
chanel-bofa <52410735+chanel-bofa@users.noreply.github.com>
2019-07-03 09:23:40 -0400

    Create des_resp.js

---
+++ js/des_resp.js
@@ -0,0 +1,21 @@
+var sqa_tester_d = "Provide input on the development of test plans, scenarios, and test cases"+ "<br/>";
+var sqa_tester_r = "- Execute test plans and identify, record, and report bugs and issues in a timely manner" + "<br/>"
+					+ "- Communicate test progress, test results, and other relevant information to project stakeholders" + "<br/>"
+					+ "- Devlop an understanding of the application and project goals to design script and execute manula and automated software testing" + "<br/>";
+var sqa_lead_d = "Manages contractors and consultants, and oversees the development, maintenance, enhancement, and execution of software QA artifacts within a line of business or technology function";
+var sqa_lead_r = "- Assess and evaluate risk from a timing and priority perspective" + "<br/>"
+		+ "- Knowledge of the budget(e.g. delivering within budget)" + "<br/>" 
+		+ "- Exercise agility in adapting to changes in scope" + "<br/>"
+		+ "- Captain on the Field, directing the activities of a team and providing \"\" " + "<br/>"
+		+ "- Work with business partners, vendors, operations, and technology to ensure that applications are developed properly and ready for deployment" + "<br/>";
+var sqa_lead_s_d = "Manages Contractors and consultants, and works with business partners, vendors, operations, and technology to ensure that applications are developed properly and ready for deployment"+ "<br/>";
+var sqa_lead_s_r = "- Apply strategic thinking to complex, large-scale projects " + "<br/>"
+		+ "- Exercise agility in adapting to changes in scope" + "<br/>"
+		+ "- Captain on the Field, directing the activities of a large QA team or several teams and providing \"on the spot\" coaching and mentoring on QA testing techniques" + "<br/>";
+var sqa_manager_d = "Manages and influences a team of employees toward a common goal and/or deliverable"+ "<br/>";
+var sqa_manager_r = "- Develop employees through regular performance management and coaching routines" + "<br/>"
+		+ "- Drive QA metrics and dashboards" + "<br/>";
+var sqa_manager_s_d = "Develop employees through regular performance management and coaching routines, and drives QA metrics and dashboards"+ "<br/>";
+var sqa_manager_s_r = "- Exercise agility in adapting to changing scope, budget, etc." + "<br/>"
+		+ "- Serve as a role model in fostering an inclusive, client-centric team environment aligned to our guiding principles and strategic priorities" + "<br/>"
+		+ "- Structure, motivate, and influence multiple teams" + "<br/>";		
